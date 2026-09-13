40aab7a4c5a6f9d2cc494f6aca2633792b48194b
RustHalo <Adele.Alsaed@Student.HTW-Berlin.de>
2026-09-13 17:50:19 +0200

    Read operation code + "dummy" item

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ function getListings() {
 //func. to save listings to browser storage
 function saveListings(listingsArray) {
     //json turns objects to text to survive page reload
-    localStorage.setItem('marketplace:listings', JSON.stringify(listingsArray))
+    localStorage.setItem('marketplace_listings', JSON.stringify(listingsArray));
 }
 
 
@@ -25,8 +25,65 @@ function saveListings(listingsArray) {
 
 //wait for html to fully load before running logic
 $(document).ready(function() {
+
+    renderListings();
+
     $('#role-switch').on('change', function() {
         let currentRole = $(this).val();
         console.log("Current role is now: " + currentRole);
     });
 });
+
+//initial data (seed data)
+// if storage is empty, add 1dummy item to have something to read
+let currentListings = getListings();
+if (currentListings.length === 0) {
+    let dummyListing = {
+        id: Date.now(),
+        title: "Calculus Textbook",
+        price: 45.00,
+        category: "Textbooks",
+        description: "Barely used, no highlighting or writings.",
+        author: "Member"
+    };
+    //push method to add new item to array
+    currentListings.push(dummyListing);
+    saveListings(currentListings);
+}
+
+//the Read operation
+function renderListings() {
+    let listings = getListings();
+
+    let grid = document.getElementById('listings-grid');
+    grid.innerHTML = '';
+
+    listings.forEach((item) => {
+
+        let card = document.createElement('div');
+        card.classList.add('controls');
+
+        let title = document.createElement('h3');
+        title.textContent = item.title;
+
+        let price = document.createElement('p');
+        price.textContent = "Price: €" + item.price;
+
+        let category = document.createElement('p');
+        category.textContent = "Category: " + item.category;
+
+        let author = document.createElement('p');
+        author.textContent = "Seller: " + item.author;
+
+        let description = document.createElement('p');
+        description.textContent = "Description: " + item.description;
+
+        card.appendChild(title);
+        card.appendChild(price);
+        card.appendChild(category);
+        card.appendChild(author);
+        card.appendChild(description);
+        
+        grid.appendChild(card);
+    });
+}
